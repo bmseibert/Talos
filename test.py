3337852159952bef73b1c012832ba4ba669a14b9
bmseibert <bmseibert@wpi.edu>
2020-10-13 22:19:14 -0400

    Can Connect to Drone

---
+++ test.py
@@ -0,0 +1,18 @@
+import time
+from Talos import Tello
+
+
+class Test:
+    def __init__(self):
+        self.drone = Tello.Tello()
+
+    def connection_test(self):
+        self.drone.send_command('command')
+        # time.sleep(2)
+        self.drone.send_command('takeoff')
+        time.sleep(2)
+        self.drone.send_command('land')
+
+
+Test1 = Test()
+Test1.connection_test()
